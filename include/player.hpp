8d607b3c591b089cd2c561380f5e140a1d7ae8fb
Rjotaro <genkiginchan@gmail.com>
2021-01-28 17:21:24 +0500

    Refactoring command section

--- include/player.hpp
+++ include/player.hpp
@@ -3,23 +3,30 @@
 
 #include <iostream>
 #include <memory>
+#include <functional>
 
 #include "home.hpp"
 
 class Cat;
 
-class Player {
-    public:
+class Player : std::enable_shared_from_this<Player> {
+public:
 
     inline static auto home_ = Home::getInstance(); // inline because without it doen't work
 
     Player();
-
-    void inputCommand();
-    void findCatsInHome();
-    void playWithCat(std::shared_ptr<Cat>);
-    std::shared_ptr<Cat> selectCatByName(std::string);
-    void fillCatsBow();
+    
+    inline std::shared_ptr<Player> get_instance();
+    void                           findCatsInHome();
+    std::shared_ptr<Cat>           selectCat(std::string);
+    void                           playWithCat();
+//  void fillCatsBow();
 };
+
+inline void input_command(std::shared_ptr<Player> player);
+inline void command_help(std::shared_ptr<Player> player);
+inline void command_find(std::shared_ptr<Player> player);
+inline void command_play(std::shared_ptr<Player> player);
+inline void command_exit(std::shared_ptr<Player> player);
 
 #endif
--- src/player.cpp
+++ src/player.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <unordered_map>
 
 #include "cats.hpp"
 #include "home.hpp"
@@ -6,62 +7,48 @@
 
 extern std::mutex catMutex;
 
+std::unordered_map<std::string, std::function<void(std::shared_ptr<Player>)>> commands {
+    {"help", command_help},
+    {"find", command_find},
+    {"play", command_play},
+    {"exit", command_exit}
+};
+
 Player::Player() {
-    inputCommand();
+    input_command(get_instance());
 }
 
-void Player::inputCommand() {
-    std::cout << "Please, enter the commands ((help) for list of all commands):" << std::endl;
-    while(true) {
-        std::cout << ": ";
-        std::string command;
-        std::cin >> command;
+inline std::shared_ptr<Player> Player::get_instance() {
+    return shared_from_this();
+}
 
-        if(command == "help") {
-            std::cout << "  (find) all cats in home." << std::endl;
-            std::cout << "  (play) with cat." << std::endl;
-            std::cout << "  (bow)'s food level." << std::endl;
-            std::cout << "  (fill) cat's bow" << std::endl;
-            std::cout << "  (clear) screen." << std::endl;
-            std::cout << "  (exit) from game." << std::endl;
-        } else if (command == "find") {
-            findCatsInHome();
-        } else if (command == "play") {
-            catMutex.lock();
-            std::cout << "  Input cat name: ";
-            std::string name;
-            std::cin >> name;
-            playWithCat(selectCatByName(name));
-            catMutex.unlock();
-        } else if (command == "bow") {
-            std::cout << "  Level of food in bow: " << home_->catFood_ << std::endl;
-        } else if (command == "fill") {
-            fillCatsBow();
-            std::cout << "  Bow now is full!" << std::endl;
-        } else if (command == "clear") {
-            std::cout << "\x1B[2J\x1B[H"; // Yeah, it's clean terminal on Linux/bash.
-        } else if (command == "exit") {
-            std::exit(EXIT_SUCCESS);
-        } else {
-            std::cout << "  Incorrect command, try again." << std::endl;
-        }
+inline void input_command(std::shared_ptr<Player> player) {
+    std::cout << "Please, enter the commands ((help) for list of all commands):" << std::endl;
+    
+    std::string command;
+    while(std::cin >> command) {
+        commands[command](player);
     }
 }
 
 void Player::findCatsInHome() {
     if(home_->catsInHome_.size() != 0) {
-            for(auto i : home_->catsInHome_) {
-                std::cout << "  " << i->name_ << std::endl;
-            }
-    } else std::cout << "   No cats in our home :c" << std::endl;
+        for(auto i : home_->catsInHome_) std::cout << "  " << i->name_ << std::endl;
+    }
+    else std::cout << "   No cats in our home :c" << std::endl;
 }
 
-void Player::playWithCat(std::shared_ptr<Cat> catPtr) {
+void Player::playWithCat() {
+    std::cout << "  Input cat name: ";
+    std::string name;
+    std::cin >> name;
+    
+    auto catPtr = selectCat(name);
     if(catPtr != nullptr) catPtr->makeCatSound();
     else std::cout << "    Can't find cat." << std::endl;
 }
 
-std::shared_ptr<Cat> Player::selectCatByName(std::string name) {
+std::shared_ptr<Cat> Player::selectCat(std::string name) {
     if(home_->catsInHome_.size() != 0) {
         for(auto i : home_->catsInHome_) {
             if(i->name_ == name) return i;
@@ -70,6 +57,29 @@ std::shared_ptr<Cat> Player::selectCatByName(std::string name) {
     return nullptr;
 }
 
-void Player::fillCatsBow() {
+/*void Player::fillCatsBow() {
     home_->catFood_ = 100;
+}*/
+
+inline void command_help(std::shared_ptr<Player> player) {
+    std::cout << "  (find) all cats in home." << std::endl;
+    std::cout << "  (play) with cat." << std::endl;
+    std::cout << "  (bow)'s food level." << std::endl;
+    std::cout << "  (fill) cat's bow" << std::endl;
+    std::cout << "  (clear) screen." << std::endl;
+    std::cout << "  (exit) from game." << std::endl;
+}
+
+inline void command_find(std::shared_ptr<Player> player) {
+    player->findCatsInHome();
+}
+
+inline void command_play(std::shared_ptr<Player> player) {
+    catMutex.lock();
+    player->playWithCat();
+    catMutex.unlock();
+}
+
+inline void command_exit(std::shared_ptr<Player> player) {
+    std::exit(EXIT_SUCCESS);
 }
